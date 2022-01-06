deea32b4503450c94f55eba7ae46988062b66e12
Julia Skor <jonatickilljoy@gmail.com>
2022-01-06 19:01:40 +0300

    added telegram link and deleted extra files

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import avatar from '../images/avatar.png';
-import mail from '../images/Mail.svg';
+import telegram from '../images/telegram.png';
 
 export default function Info() {
     return (
         <div className="header">
             <img className="avatar" src={avatar} />
             <h1 className="name-title">Julia Skorobogatova</h1>
-            <p className="job-name">Junior Frontend Developer</p>
+            <p className="job-name">Intern/Junior Frontend Developer</p>
             <a className="website" target="_blank" href= "https://github.com/ulythegod">https://github.com/ulythegod</a>
             <div className="buttons">
-                <div className="button email">
-                    <a target="_blank" href="mailto:mcrjbddl30mars@gmail.com"><img src={mail}/> Email</a>
+                <div className="button telegram">
+                    <a target="_blank" href="https://t.me/ulythevoland"><img src={telegram}/> Telegram</a>
                 </div>
             </div>
         </div>
